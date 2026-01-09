baa64eab2e7373a9e6824921c074f52487384577
MykeCv <mykecevallos@gmail.com>
2026-01-08 19:01:57 -0500

    Taller 20

--- src/components/PokemonOpciones.vue
+++ src/components/PokemonOpciones.vue
@@ -1,35 +1,40 @@
 <template>
   <div class="opciones-container">
     <ul>
-        <li>Pokemon 1</li>
-        <li>Pokemon 2</li>
-        <li>Pokemon 3</li>
-        <li>Pokemon 4</li>
+      <li v-on:click="pasarPadre(pokemon.id)" v-for="pokemon in listaPokemons" :key="pokemon.id">{{ pokemon.nombre }}</li>
     </ul>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  props: {
+    listaPokemons: {
+      type: Array,
+      required: true,
+    },
+  },
+  methods: {
+    pasarPadre(id) {
+      console.log(id);
+      this.$emit("seleccionado", id);
+    },
+  }
+};
 </script>
 
 <style>
-
-.opciones-container{
+ul {
+  list-style-type: none;
 }
-    ul{
-        list-style-type: none;
-    }
-    li{
-        background-color: white;
-        border-radius: 5px;
-        border: 1px solid;
-        cursor: pointer;
-        width: 250px;
-        margin-top: 10px;
-        text-align: center;
-    }
 
+li {
+  background-color: white;
+  border-radius: 5px;
+  border: 1px solid;
+  cursor: pointer;
+  width: 250px;
+  margin-top: 10px;
+  text-align: center;
+}
 </style>
--- src/views/PokemonView.vue
+++ src/views/PokemonView.vue
@@ -1,20 +1,54 @@
 <template>
-    <PokemonImagen :pokemonId="1"/>
-    <PokemonOpciones/>
+    <h1 v-if="ganador">Correcto! Ganaste</h1>
+    <h1 v-if="perdedor">Incorrecto! Vuelve a intentarlo</h1>
+    <PokemonImagen :pokemonId="pokemonGanador"/>
+    <PokemonOpciones @seleccionado="evaluarGanador($event)" :listaPokemons="pokemonArr" />
 </template>
 
 <script>
-import PokemonImagen from '../components/PokemonImagen.vue' 
+import PokemonImagen from '../components/PokemonImagen.vue'
 import PokemonOpciones from '../components/PokemonOpciones.vue'
+import { obtenerVectorPokemonFachada, obtenerAleatorioFachada } from "../clients/PokemonClient"
 
 export default {
-    components:{
+    components: {
         PokemonImagen,
         PokemonOpciones,
-    }
-}
+    },
+    data() {
+        return {
+            pokemonArr: [],
+            pokemonGanador: null,
+            ganador: false,
+            perdedor: false,
+        };
+    },
+    mounted() {
+        console.log('Componente montado');
+        this.iniciarJuego();
+    },
+    methods: {
+        async iniciarJuego() {
+            this.pokemonArr = await obtenerVectorPokemonFachada();
+
+            const idAleatorio = obtenerAleatorioFachada(0, 3);
+            this.pokemonGanador = this.pokemonArr[idAleatorio].id;
+        },
+        evaluarGanador(idGanador) {
+            console.log('Valor recibido desde padre');
+            console.log(idGanador);
+            if(idGanador === this.pokemonGanador){
+                console.log('Ganador');
+                this.ganador = true;
+                return;
+            }else{
+                console.log('Perdedor');
+                this.perdedor = true;
+                return;
+            }
+        }
+    },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
